72cf29e1f2a2fbb78f9ff3a0be300908b6eb5ee0
MT Rieg (vscode) <mtrieg@outlook.com>
2025-06-02 15:05:35 -0400

    Making the file names stored in their own file

---
+++ outlines_test/file-names.js
@@ -0,0 +1,9 @@
+// file-names.js
+export const files = {
+  outlineImage: "outline.png",
+  referenceImage: "clickable.png",
+  baseImage: "no-outline.png",
+  paletteOverlay: "overlay.png",
+  palettesJson: "palettes.json",
+  mappingsJson: "mappings.json"
+};
