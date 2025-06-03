c3b90a95bc6be8625d4edb8da77f04f12d3f7675
MT Rieg (vscode) <mtrieg@outlook.com>
2025-06-03 00:20:44 -0400

    using the WOC map, and updating the default redirect

---
+++ WOC_test/file-names.js
@@ -0,0 +1,9 @@
+// file-names.js
+export const files = {
+  outlineImage: "./files/outline.png",
+  referenceImage: "./files/clickable.png",
+  baseImage: "./files/no-outline.png",
+  paletteOverlay: "./files/overlay.png",
+  palettesJson: "./files/palettes.json",
+  mappingsJson: "./files/mappings.json"
+};
