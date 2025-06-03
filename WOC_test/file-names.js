aef389559c1070940eaca939fc8043c2700b28eb
MT Rieg (vscode) <mtrieg@outlook.com>
2025-06-03 14:56:35 -0400

    renaming

--- WOC_test/file-names.js
+++ WOC_test/file-names.js
@@ -3,7 +3,7 @@ export const files = {
   outlineImage: "./files/outline.png",
   referenceImage: "./files/clickable.png",
   baseImage: "./files/no-outline.png",
-  paletteOverlay: "./files/overlay.png",
+  paletteOverlay: "./files/palette.png",
   palettesJson: "./files/palettes.json",
   mappingsJson: "./files/mappings.json"
 };
