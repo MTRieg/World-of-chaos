222440cd51a31f409cab1a4ede515a10c73c3d09
MT Rieg (vscode) <mtrieg@outlook.com>
2025-06-03 15:18:15 -0400

    style

--- WOC_test/file-names.js
+++ WOC_test/file-names.js
@@ -3,7 +3,7 @@ export const files = {
   outlineImage: "./files/outline.png",
   clickableImage: "./files/clickable.png",
   baseImage: "./files/no-outline.png",
-  SwapRefImage: "./files/swap-ref.png",
+  swapRefImage: "./files/swap-ref.png",
   palettesJson: "./files/palettes.json",
   mappingsJson: "./files/mappings.json"
 };
