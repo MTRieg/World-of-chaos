fce7015201850e26dac47786a9306003c24ba985
MT Rieg (vscode) <mtrieg@outlook.com>
2025-06-03 23:10:35 -0400

    github being weird

--- WOC_test/file-names.js
+++ WOC_test/file-names.js
@@ -8,12 +8,3 @@ export const files = {
   mappingsJson: "./files/mappings.json"
 };
 
-// file-names.js
-export const files = {
-  outlineImage: "./files/outline.png",
-  clickableImage: "./files/clickable.png",
-  baseImage: "./files/no-outline.png",
-  swapRefImage: "./files/swap-ref.png",
-  palettesJson: "./files/palettes.json",
-  mappingsJson: "./files/mappings.json"
-};
