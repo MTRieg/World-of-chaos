ff256dc9627ebe73a8cf7269bcce3697aea8a3b2
MTRieg <168741732+MTRieg@users.noreply.github.com>
2025-06-07 00:24:09 -0400

    revert last change

--- WOC_demo/file-names.js
+++ WOC_demo/file-names.js
@@ -5,5 +5,5 @@ export const files = {
   baseImage: "./files/no-outline_20250607_013758.png",
   swapRefImage: "./files/swap-ref_20250607_013758.png",
   palettesJson: "./files/palettes.json",
-  mappingsJson: "./files/mappings_20250607_041621.json",
+  mappingsJson: "./files/mappings_20250607_003137.json",
 };
