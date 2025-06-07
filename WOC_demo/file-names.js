ef3484ea46222b2b71a4759fa0e9481e8c36bc72
MT Rieg (vscode) <mtrieg@outlook.com>
2025-06-07 17:28:33 -0400

    deleting archive, making the verified test now work in demo

--- WOC_demo/file-names.js
+++ WOC_demo/file-names.js
@@ -1,9 +1,9 @@
 // file-names.js
 export const files = {
-  outlineImage: "./files/outline.png",
-  clickableImage: "./files/clickable.png",
-  baseImage: "./files/no-outline.png",
-  swapRefImage: "./files/swap-ref.png",
+  outlineImage: "./files/outline_20250607_212040.png",
+  clickableImage: "./files/clickable_20250607_212040.png",
+  baseImage: "./files/no-outline_20250607_212040.png",
+  swapRefImage: "./files/swap-ref_20250607_212040.png",
   palettesJson: "./files/palettes.json",
   mappingsJson: "./files/mappings_20250607_182138.json",
 };
--- WOC_test_archive/file-names.js
+++
@@ -1,9 +0,0 @@
-// file-names.js
-export const files = {
-  outlineImage: "./files/outline_20250604_054122.png",
-  clickableImage: "./files/clickable_20250604_054122.png",
-  baseImage: "./files/no-outline_20250604_054122.png",
-  swapRefImage: "./files/swap-ref_20250604_054122.png",
-  palettesJson: "./files/palettes.json",
-  mappingsJson: "./files/mappings_20250606_234556.json",
-};
