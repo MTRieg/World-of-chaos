f5b330fdf241f03917e7c147760d3f3c8402d1de
MT Rieg (vscode) <mtrieg@outlook.com>
2025-06-07 20:08:50 -0400

    test to demo

--- WOC_demo/file-names.js
+++ WOC_demo/file-names.js
@@ -1,9 +1,9 @@
 // file-names.js
 export const files = {
-  outlineImage: "./files/outline_20250607_212040.png",
-  clickableImage: "./files/clickable_20250607_212040.png",
-  baseImage: "./files/no-outline_20250607_212040.png",
-  swapRefImage: "./files/swap-ref_20250607_212040.png",
+  outlineImage: "./files/outline_20250608_000434.png",
+  clickableImage: "./files/clickable_20250608_000434.png",
+  baseImage: "./files/no-outline_20250608_000434.png",
+  swapRefImage: "./files/swap-ref_20250608_000434.png",
   palettesJson: "./files/palettes.json",
-  mappingsJson: "./files/mappings_20250607_182138.json",
+  mappingsJson: "./files/mappings_20250607_233556.json",
 };
