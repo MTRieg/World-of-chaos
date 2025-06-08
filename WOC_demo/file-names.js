460d262261c4ffc70e4e2edf60c9eaed307aa8d3
MT Rieg (vscode) <mtrieg@outlook.com>
2025-06-08 13:43:45 -0400

    test to demo (one color was not fixed)

--- WOC_demo/file-names.js
+++ WOC_demo/file-names.js
@@ -1,9 +1,9 @@
 // file-names.js
 export const files = {
-  outlineImage: "./files/outline_20250608_000434.png",
-  clickableImage: "./files/clickable_20250608_000434.png",
-  baseImage: "./files/no-outline_20250608_000434.png",
-  swapRefImage: "./files/swap-ref_20250608_000434.png",
+  outlineImage: "./files/outline_20250608_173611.png",
+  clickableImage: "./files/clickable_20250608_173611.png",
+  baseImage: "./files/no-outline_20250608_173611.png",
+  swapRefImage: "./files/swap-ref_20250608_173611.png",
   palettesJson: "./files/palettes.json",
   mappingsJson: "./files/mappings_20250607_233556.json",
 };
