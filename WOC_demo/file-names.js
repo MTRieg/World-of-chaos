793844d1cb8ac9c7b6342899fd077574822912f4
MTRieg <168741732+MTRieg@users.noreply.github.com>
2025-06-10 17:36:56 -0400

    Upload multiple files in a single commit

--- WOC_demo/file-names.js
+++ WOC_demo/file-names.js
@@ -1,9 +1,9 @@
 // file-names.js
 export const files = {
-  outlineImage: "./files/outline_20250608_202520.png",
-  clickableImage: "./files/clickable_20250608_202520.png",
-  baseImage: "./files/no-outline_20250608_202520.png",
-  swapRefImage: "./files/swap-ref_20250608_202520.png",
+  outlineImage: "./files/outline_20250610_213650.png",
+  clickableImage: "./files/clickable_20250610_213650.png",
+  baseImage: "./files/no-outline_20250610_213650.png",
+  swapRefImage: "./files/swap-ref_20250610_213650.png",
   palettesJson: "./files/palettes.json",
   mappingsJson: "./files/mappings_20250610_161338.json",
 };
